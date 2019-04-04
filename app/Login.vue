eef56561d9cffb4a64e0acc4ef31a71a1f3cc8e5
Hugo Camboulive <hugo@camboulive.me>
2019-04-04 20:54:31 +0200

    vue: add login page

---
+++ app/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="login">
+        <h1>SpotRepeater</h1>
+        <p>Loop parts of </p>
+        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+};
+</script>
